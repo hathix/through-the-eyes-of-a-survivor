f398f89409692317b5abd5dde76d9ff77726d07b
Neel Mehta <neelmehta@college.harvard.edu>
2016-11-14 16:21:30 -0500

    beautify

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var margin = {
 };
 
 var width = 960 - margin.left - margin.right,
-     height = 500 - margin.top - margin.bottom;
+  height = 500 - margin.top - margin.bottom;
 
 noUiSlider.create($('#count-slider')
   .get(0), {
