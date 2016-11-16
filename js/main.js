b1e2e5a7e3b600e70c154124be0409c3e1ed4b91
Neel Mehta <neelmehta@college.harvard.edu>
2016-11-16 17:37:01 -0500

    add stories of survivors

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 // initialize libraries
-noUiSlider.create($('#count-slider')
-  .get(0), {
-    start: [2001, 2001],
-    step: 1,
-    connect: true,
-    range: {
-      min: [2001],
-      max: [2013]
-    },
-    pips: { // Show a scale with the slider
-      mode: 'steps',
-      stepped: true,
-      density: 100
-    }
-  });
+// noUiSlider.create($('#count-slider')
+//   .get(0), {
+//     start: [2001, 2001],
+//     step: 1,
+//     connect: true,
+//     range: {
+//       min: [2001],
+//       max: [2013]
+//     },
+//     pips: { // Show a scale with the slider
+//       mode: 'steps',
+//       stepped: true,
+//       density: 100
+//     }
+//   });
 
 $(function() {
   $('#fullpage')
