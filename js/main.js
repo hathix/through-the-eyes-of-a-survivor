db9503078ca0e45cace98c02d3863c7637506e73
Neel Mehta <neelmehta@college.harvard.edu>
2016-11-20 17:19:29 -0500

    start typewriting on page load

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+var stories;
+d3.json("data/stories.json", function(error, data) {
+  stories = data;
+
+  // create a typewriter for each story
+  stories.forEach(function(story) {
+    story.typewriter = malarkey(document.querySelector("#" + story.id), {
+      typeSpeed: 25,
+      setter: function(elem, val) {
+        // replace "\n" with line breaks
+        val = val.replace(/\n/, "<br><br>");
+        elem.innerHTML = val;
+      }
+    });
+  });
+});
+
 // initialize libraries
 // noUiSlider.create($('#count-slider')
 //   .get(0), {
@@ -21,29 +38,41 @@ $(function() {
       //   scrollingSpeed: 500,
       //   scrollBar: true,
       navigation: true,
-      anchors: ['anchor-section-intro'],
-      menu: '#main-menu'
+      //   anchors: ['anchor-section-intro'],
+      menu: '#main-menu',
+
+      afterLoad: function(anchorLink, index) {
+        // start typewriting each story once you visit it
+        if (index === 2) {
+          typewrite(stories[0]);
+        }
+        if (index === 3) {
+          typewrite(stories[1]);
+        }
+      },
+
+      onLeave: function(index, nextIndex) {
+        // pause typewriting once you leave
+        // DOESNT WORK RN b/c the typewriters just keep ticking once you start them - hard to stop
+        // if (index === 2) {
+        //   stories[0].typewriter.pause();
+        // }
+        // if (index === 3) {
+        //   stories[1].typewriter.pause();
+        // }
+      }
     });
 });
 var lineChart = new LineChart("police-reports");
 var crimeRate = new CrimeRate();
 var survivors = new Survivors("affected", 20, 100);
 
-d3.json("data/stories.json", function(error, data) {
 
-  // each element of this array is a story
-  data.forEach(function(story) {
-    var typewriter = malarkey(document.querySelector("#" + story.id), {
-      typeSpeed: 25,
-      setter: function(elem, val) {
-        // replace "\n" with line breaks
-        val = val.replace(/\n/, "<br><br>");
-        elem.innerHTML = val;
-      }
-    });
-    story.lines.forEach(function(line) {
-      typewriter.type(line + "\n")
-        .pause(1500);
-    });
+
+// each element of this array is a story
+var typewrite = function(story) {
+  story.lines.forEach(function(line) {
+    story.typewriter.type(line + "\n")
+      .pause(1500);
   });
-});
+}
